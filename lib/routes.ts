59122fe51e60bd162a13d8c4fc9a6cafd31e5470
danieltorscho <daniel.torscho@gmail.com>
2024-09-18 23:27:18 +0200

    routes

---
+++ lib/routes.ts
@@ -0,0 +1,29 @@
+interface IRoute {
+  name: string;
+  path: string;
+  method:
+    | "GET"
+    | "POST"
+    | "PUT"
+    | "DELETE"
+    | "PATCH"
+    | "HEAD"
+    | "OPTIONS"
+    | "CONNECT"
+    | "TRACE";
+  handler: (req?: Request) => Promise<Response> | Response;
+}
+
+class Route {
+  public list: IRoute[];
+
+  constructor() {
+    this.list = [];
+  }
+
+  public define(list: IRoute[]) {
+    this.list = list;
+  }
+}
+
+export default Route;
---
+++ tests/router.test.ts
@@ -0,0 +1,71 @@
+import { expect, it } from "bun:test";
+import Routes from "../lib/routes";
+
+it("should be able to define routes", () => {
+  // Given
+  const routes = new Routes();
+
+  // When
+  routes.define([
+    {
+      name: "test.route1",
+      method: "GET",
+      path: "/route1",
+      handler: () => {
+        return new Response("route1");
+      },
+    },
+    {
+      name: "test.route2",
+      method: "GET",
+      path: "/route2",
+      handler: () => {
+        return new Response("route2");
+      },
+    },
+  ]);
+
+  // Then
+  expect(routes.list[0].name).toBe("test.route1");
+  expect(routes.list[1].name).toBe("test.route2");
+  expect(routes.list.length).toBe(2);
+});
+
+it("should handle route action in json format", async () => {
+  // Given
+  const routes = new Routes();
+  routes.define([
+    {
+      name: "test",
+      method: "GET",
+      path: "/test",
+      handler: () => {
+        return Response.json({ message: "tested" });
+      },
+    },
+  ]);
+
+  // When
+  const action = await routes.list[0].handler();
+
+  // Then
+  expect(action.status).toBe(200);
+  expect(action.headers.toJSON()["content-type"]).toBe(
+    "application/json;charset=utf-8"
+  );
+});
+
+// it.skip("responds to GET route with 200", async () => {
+//   // Given
+//   const router = null;
+//   router.get("/test", (req, res) => {
+//     res.statusCode(200);
+//     res.send("test page");
+//   });
+
+//   // When
+//   const request = await fetch("http://localhost:3000/test");
+
+//   // Then
+//   expect(request.statusCode).toBe(200);
+// });
